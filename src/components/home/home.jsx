34b89d3c59468f5f4ee34c6cafcf578ecd8f3b92
hydrax117 <pauljoseph5000@gmail.com>
2023-08-04 00:51:16 +0100

    made some changes

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from "react";
 import ThemeContext from "../../infrastructure/themeContex/themeContext";
 import { Footer } from "../footer/footer";
+
 export const Home = () => {
-  const theme = useContext(ThemeContext);
   return (
     <>
       <div>
-        <h1 style={{ fontSize: theme.fontSizes.h2 }}>this is the kHome</h1>
+        <h1>this is the kHome</h1>
       </div>
 
       <Footer />
